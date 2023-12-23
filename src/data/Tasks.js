93fc3ded3e50155c6ff5f5df1e7d796343f348cc
lopezperezjc <lopezperez@gmail.com>
2023-12-23 03:38:24 -0600

    first commit

---
+++ src/data/Tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Mi primera tarea',
+        description: 'la descripcion de mi tarea',
+    },
+    {
+        id: 1,
+        title: 'Mi segunda tarea',
+        description: 'la descripcion de mi tarea',
+    },
+    {
+        id: 2,
+        title: 'Mi tercera tarea',
+        description: 'la descripcion de mi tarea',
+    },
+]
